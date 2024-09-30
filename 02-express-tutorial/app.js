d4a09e363fcbadb4640748d0cc43a9763679efc3
duc <ducle@teqnological.asia>
2024-09-30 09:20:16 +0700

    Fourth commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const logger = require("./logger.js");
+const authorize = require("./authorize.js");
+let { people } = require("./data.js");
 
-app.get("/", (req, res) => {
-  res.json([{ name: "duc" }, { name: "le" }]);
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({extended: true}));
+
+app.get("/about/duc", (req, res) => {
+  res.status(200).send({ success: true, data: people });
 });
 
-app.listen(5000, (req, res) => {
+app.post("/login", (req, res) => {
+  const {name} = req.body
+  if(name){
+    console.log(name);
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  // console.log(name);
+  res.status(401).send('Please provide credential');
+});
+
+app.listen(5000, () => {
   console.log("server is listening on port 5000...");
 });

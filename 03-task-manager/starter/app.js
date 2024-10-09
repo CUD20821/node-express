e4874780e01f4b15a02f612c460dfdc3616d508a
duc <ducle@teqnological.asia>
2024-10-09 16:20:48 +0700

    Create basic appjs, controllers, routes files

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require('./routes/tasks')
+
+const port = 3000;
+// middlewares
+
+// routes
+app.use('/api/v1/tasks', tasks)
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,5 @@
+const getTasks = (req, res) => {
+  res.send('Hello')
+}
+
+module.exports = {getTasks}

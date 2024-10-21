940ee83139b04c91272b070fb735bc39a93e1f85
duc <letrungducshizuoka@gmail.com>
2024-10-21 23:38:48 +0700

    response with many queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,31 @@
+const Product = require('../models/product')
+
 const getAllProductStatic = async (req, res, next) => {
-  throw new Error('testing async errors')
-  res.status(200).json({msg: 'product testing route'})
+  const search = 'duc'
+  const products = await Product.find({
+    name: {$regex: search, $options: 'i'},
+  })
+  res.status(200).json({ products, nbHits: products.length })
+
 }
 
 const getAllProducts = async (req, res, next) => {
-  res.status(200).json({msg: 'product route'})
+  const { featured, company, name } = req.query
+  const queryObj = {}
+  if (featured) {
+    queryObj.featured = featured === 'true' ? true : false
+  }
+
+  if (company) {
+    queryObj.company = company
+  }
+
+  if (name) {
+    queryObj.name = {$regex: name, $options: 'i'}
+  }
+  console.log(queryObj)
+  const products = await Product.find(queryObj)
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {

23de7e4e9be975007ff38ce2d05db149d90c8ecc
duc <letrungducshizuoka@gmail.com>
2024-10-23 00:36:24 +0700

    final code for store api

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,16 +1,17 @@
 const Product = require('../models/product')
 
 const getAllProductStatic = async (req, res, next) => {
-  const search = 'duc'
-  const products = await Product.find({
-    name: {$regex: search, $options: 'i'},
-  })
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort('name')
+    .select('name price')
+    .limit(4)
+    .skip(1)
   res.status(200).json({ products, nbHits: products.length })
 
 }
 
 const getAllProducts = async (req, res, next) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObj = {}
   if (featured) {
     queryObj.featured = featured === 'true' ? true : false
@@ -21,10 +22,50 @@ const getAllProducts = async (req, res, next) => {
   }
 
   if (name) {
-    queryObj.name = {$regex: name, $options: 'i'}
+    queryObj.name = { $regex: name, $options: 'i' }
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': 'eq',
+      '<': '$lt',
+      '<=': '$lte'
+    }
+    const regEx = /\b(<|>|>=|=|<=)\b/g
+    let filter = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+    const options = ['price', 'rating']
+    filter = filter.split(',').forEach((item) => {
+        const [field, operator, value] = item.split('-')
+        if(options.includes(field)){
+          queryObj[field] ={[operator]: Number(value)}
+        }
+    })
+  }
+
+
   console.log(queryObj)
-  const products = await Product.find(queryObj)
+  let result = Product.find(queryObj) // Create query Object, not execution to DB
+  if (sort) {
+    const sortList = sort.split(',').join(' ') // Mongoose required query have to be a string, if there more than 1 query have been passed, it has to be differentiated by space 
+    result = result.sort(sortList)
+  }
+  else {
+    result = result.sort('createAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+  const products = await result // Execute to DB
   res.status(200).json({ products, nbHits: products.length })
 }
 
